a03ced9754fd5b7a0e15e0d4ac233a0a73ab9f72
imperialiano147-lab <imperialiano147@gmail.com>
2025-10-30 21:02:05 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBtn").addEventListener("click", () => {
+  alert("Login page coming soon!");
+});
+
+document.getElementById("registerBtn").addEventListener("click", () => {
+  alert("Registration page coming soon!");
+});
